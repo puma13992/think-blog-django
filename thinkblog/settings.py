caa46f7311f92f150c2d6c069fd9b08ae180038e
puma13992 <>
2023-08-28 13:22:59 +0000

    Allow host with correct url

--- thinkblog/settings.py
+++ thinkblog/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['thinkblog-django.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['thinkblog-django-9e828dfbb607.herokuapp.com', 'localhost']
 
 
 # Application definition
