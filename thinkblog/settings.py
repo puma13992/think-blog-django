1215735d4d68cdc9f295efda22d48bc452db2d5a
puma13992 <>
2023-08-28 13:43:35 +0000

    Add models for database

--- thinkblog/settings.py
+++ thinkblog/settings.py
@@ -29,7 +29,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['thinkblog-django-9e828dfbb607.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['thinkblog-django-9e828dfbb607.herokuapp.com', 'localhost',
+                 '8000-puma13992-think-blog-dja-936dicf3d0.us2.codeanyapp.com']
 
 
 # Application definition
